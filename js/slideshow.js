10654a57f747e72c607550f1ab4f905bfb9c4569
footya <footya@gmail.com>
2014-09-02 20:00:42 +0800

    slide demo css

--- js/slideshow.js
+++ js/slideshow.js
@@ -3,15 +3,24 @@
  */
 define(function(require, exports, module) {
     var $ = require("jquery");
-    function SlideShow() {
+    function SlideShow(opt) {
+        this._element = opt.element;//焦点图节点
         this._status = {//状态数据
             focus: 0,
             totalNumber: 0,
         }
+        this.init();
     }
     $.extend(SlideShow.prototype,{
         init:function() {
             
+        },
+        /**
+         * 获取节点
+         */
+        getDoms:function() {
+            this._views = this._element.find("*[data-node=views]");
+            this._viewsItems = this._views.find("li");
         }
     })
     module.exports = SlideShow;
